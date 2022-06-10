0772d17f66a837bb5c88d52b04c10182293a1d3a
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-06-10 02:18:18 -0400

    Add code for creating validation set and save data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -13,7 +13,6 @@ import re
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
-
 def download_data(destination_dir, url="http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"):
 
     # Make path if does not exits
@@ -62,6 +61,7 @@ def main(input_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
+    # -------------- Download and extract data -----------------
     logger = logging.getLogger(__name__)
     # Specify root directory of project
     base_dir = input_filepath
@@ -76,15 +76,43 @@ def main(input_filepath):
     extract_data(raw_data_dir)
     logger.info('extracted data')
 
+    # -------------- Load train, test and meta data -----------------
     # Set CIFAR-10 data path 
     cifar_10_data_path = os.path.join(raw_data_dir, "cifar-10-batches-py")
     # Load train data
     train_data, train_labels = load_train_data(cifar_10_data_path) 
+    logger.info('loaded train data')
     # Load meta data
     meta_data_dict = unpickle(os.path.join(cifar_10_data_path, 'batches.meta'))
+    logger.info('loaded meta data')
     # Load test data
     test_data_dict = unpickle(os.path.join(cifar_10_data_path, 'test_batch')) 
-    print(train_data.shape, train_labels.shape)
+    logger.info('loaded test data')
+
+    # -------------- Create valid set using stratification -----------------
+    from sklearn.model_selection import train_test_split
+    X_train, X_valid, y_train, y_valid = train_test_split(train_data, train_labels, test_size=0.2, 
+                                                      stratify=train_labels, shuffle=True)
+    logger.info('splitted train data into train and valid sets using stratification')
+    X_test, y_test = test_data_dict[b'data'], np.array(test_data_dict[b'labels'])
+    
+    # -------------- Print no of instances in each set -----------------
+    print(f"Total number of training instances: {len(X_train)}")
+    print(f"Total number of validation instances: {len(X_valid)}")
+    print(f"Total number of test instances: {len(X_test)}")
+    print(f"Total number of unique classes: {len(np.unique(y_train))}") 
+
+    # -------------- Reshape X_train, X_valid and X_test to appropriate dimensions -----------------
+    X_train = X_train.reshape(X_train.shape[0],3,32,32).transpose(0,2,3,1)
+    X_valid = X_valid.reshape(X_valid.shape[0],3,32,32).transpose(0,2,3,1)
+    X_test = X_test.reshape(X_test.shape[0],3,32,32).transpose(0,2,3,1)
+
+    # -------------- Save preprocessed train, valid and test sets -----------------
+    processed_data_dir = os.path.join(base_dir, 'data/processed')
+    data_list = [X_train, y_train, X_valid, y_valid, X_test, y_test]
+    data_str_list = ['X_train', 'y_train', 'X_valid', 'y_valid', 'X_test', 'y_test']
+    for data_str, data in zip(data_str_list, data_list):
+        np.save(os.path.join(processed_data_dir, data_str), data)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
