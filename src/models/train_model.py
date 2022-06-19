8166e5ddbe6b806cf6b220e8ffda3f8c2c9bde2f
Himalaya Sharma <himalaya.sharma.1997@gmail.com>
2022-06-19 16:31:04 -0400

    Disble training for base model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -17,7 +17,7 @@ def build_model(logger, data_dict):
     logger.info('loaded pre-trained frozen VGG-16 model with imagenet weights')
 
     # Add dense layers at the end of pre-trained model
-    base_model.trainable = True
+    base_model.trainable = False
     from tensorflow.keras.models import Sequential
     from tensorflow.keras.layers import Flatten, Dense
 
